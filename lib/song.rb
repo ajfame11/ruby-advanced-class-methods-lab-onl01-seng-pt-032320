afafab09261579d5c8e4e4931d14f7f75c5e427d
aurel fontaine <ajfame11@gmail.com>
2020-04-16 20:00:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,7 +45,7 @@ class Song
   def self.new_from_filename(name)
     song = self.new
     array = name.split(" - ")
-    artist_name = name.split[0]
+    song.artist_name = name.split[0]
     song.name = name.split[1].chomp(".mp3")
     song
   end
